2da424c4958643db945264bffa20c34935e6f589
Abboude <fandf995526@gmail.com>
2024-11-08 16:55:31 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// Tesk One
+let FirstName = "mitchell";
+let lastName = "Samuel";
+let age = 25;
+let petName = "Fluffy";
+let Hobby = "football";
+let favFood = "curd";
+let occupation = "Software Developer";
+
+console.log(``the new student of our javascript class is $[FirstName] $[lastName].he is $[age] years old and he has one pet named $[petName].he likes to play $[Hobby] in the weekends and his fav food item is $[favFood]. last but not least he is a $[occupation]``);
